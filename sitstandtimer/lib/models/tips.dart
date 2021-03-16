8263f9c532d79c1d053e694dbb6464ebf0b70265
Marcos <marferp2@inf.upv.es>
2021-03-16 01:37:17 +0100

    Tips added

---
+++ sitstandtimer/lib/models/tips.dart
@@ -0,0 +1,9 @@
+class Tips {
+  static final tips = [
+    'Keep moving: regularly vary your body positioning.',
+    'Maintain good head and neck posture.',
+    'Be aware of your overall posture.',
+    'Position your chair properly.',
+    'Keep your arms close.',
+  ];
+}
