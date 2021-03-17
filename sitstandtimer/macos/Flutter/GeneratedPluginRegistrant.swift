57958b1fe6ccf27adc2d32d30e744dfff7689a7e
Marcos <marferp2@inf.upv.es>
2021-03-17 23:45:52 +0100

    Window resize

--- sitstandtimer/macos/Flutter/GeneratedPluginRegistrant.swift
+++ sitstandtimer/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,12 @@ import Foundation
 
 import audioplayers
 import path_provider_macos
+import shared_preferences_macos
+import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
 }
